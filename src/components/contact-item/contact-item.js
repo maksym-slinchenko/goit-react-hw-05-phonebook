9668618fc38a6a09632845dc6559fa2c0a6251f2
Maksym Slinchenko <max.slin777@gmail.com>
2021-03-15 23:25:11 +0200

    added state deletedName

--- src/components/contact-item/contact-item.js
+++ src/components/contact-item/contact-item.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-export default function ContactItem({ name, number, id, onRemove }) {
-  return (
-    <>
-      <li key={id} name={name}>
-        {`${name}: ${number}`}
-        <button type="button" onClick={() => onRemove(name)}>
-          Delete
-        </button>
-      </li>
-    </>
-  );
+export default class ContactItem extends Component {
+  componentWillUnmount() {
+    console.log(this.props.deletedName);
+    console.log(this.props.name);
+  }
+  render() {
+    const { name, number, id, onRemove, toSetState } = this.props;
+    return (
+      <>
+        <li key={id} name={name}>
+          {`${name}: ${number}`}
+          <button
+            type="button"
+            onClick={async () => {
+              await toSetState(name);
+              onRemove(name);
+            }}
+          >
+            Delete
+          </button>
+        </li>
+      </>
+    );
+  }
 }
-
-ContactItem.propTypes = {
-  name: PropTypes.string,
-  number: PropTypes.string,
-  id: PropTypes.string,
-};
--- src/components/contact-list/contact-list.js
+++ src/components/contact-list/contact-list.js
@@ -1,30 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import ContactItem from '../contact-item/contact-item';
-import PropTypes from 'prop-types';
 import styles from './contact-list.module.css';
 
-export default function ContactList({ contacts, onRemove }) {
-  return (
-    <>
-      <TransitionGroup component="ul" className={styles.contactList}>
-        {contacts.map(i => {
-          return (
-            <CSSTransition key={i.id} timeout={250} classNames={styles}>
-              <ContactItem
-                name={i.name}
-                number={i.number}
-                onRemove={onRemove}
-              />
-            </CSSTransition>
-          );
-        })}
-      </TransitionGroup>
-    </>
-  );
+export default class ContactList extends Component {
+  state = { deletedName: '' };
+
+  toSetDeletedName = name => this.setState({ deletedName: name });
+
+  render() {
+    const { contacts, onRemove } = this.props;
+    return (
+      <>
+        <TransitionGroup component="ul" className={styles.contactList}>
+          {contacts.map(i => {
+            return (
+              <CSSTransition
+                in={i.name !== this.state.deletedName}
+                appear={true}
+                key={i.id}
+                timeout={250}
+                classNames={styles}
+              >
+                <ContactItem
+                  deletedName={this.state.deletedName}
+                  id={i.id}
+                  name={i.name}
+                  number={i.number}
+                  onRemove={onRemove}
+                  toSetState={this.toSetDeletedName}
+                />
+              </CSSTransition>
+            );
+          })}
+        </TransitionGroup>
+      </>
+    );
+  }
 }
 
-ContactList.propTypes = {
-  contacts: PropTypes.array,
-  onRemove: PropTypes.func,
-};
+// import React from 'react';
+// import { TransitionGroup, CSSTransition } from 'react-transition-group';
+// import ContactItem from '../contact-item/contact-item';
+// import PropTypes from 'prop-types';
+// import styles from './contact-list.module.css';
+
+// export default function ContactList({ contacts, onRemove }) {
+//   return (
+//     <>
+//       <TransitionGroup component="ul" className={styles.contactList}>
+//         {contacts.map(i => {
+//           return (
+//             <CSSTransition
+//               // in={i.name !== deletedName}
+//               // appear={true}
+//               key={i.id}
+//               timeout={250}
+//               classNames={styles}
+//             >
+//               <ContactItem
+//                 // deletedName={deletedName}
+//                 name={i.name}
+//                 number={i.number}
+//                 onRemove={onRemove}
+//               />
+//             </CSSTransition>
+//           );
+//         })}
+//       </TransitionGroup>
+//     </>
+//   );
+// }
+
+// ContactList.propTypes = {
+//   contacts: PropTypes.array,
+//   onRemove: PropTypes.func,
+// };
